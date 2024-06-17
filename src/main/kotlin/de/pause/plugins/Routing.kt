ced3915d0185416e74e3efcf6f80fce7dcd1f9dd
Konrad Klawonn <konrad.klawonn@3m5.de>
2024-06-17 13:47:09 +0200

    add samples

--- src/main/kotlin/de/pause/plugins/Routing.kt
+++ src/main/kotlin/de/pause/plugins/Routing.kt
@@ -3,18 +3,16 @@ package de.pause.plugins
 import de.pause.model.Article
 import de.pause.model.ArticleRepository
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
+import java.io.File
 
 fun Application.configureRouting(articleRepository: ArticleRepository) {
     routing {
-        route("/") {
-            get {
-                val allArticles: List<Article> = articleRepository.allArticles()
-                call.respond(ThymeleafContent("index", mapOf("articles" to allArticles)))
-            }
-        }
+        staticResources("/", "static/files", index = "index.html")
         route("/article") {
             get("/all") {
                 val articles = articleRepository.allArticles()
@@ -24,6 +22,15 @@ fun Application.configureRouting(articleRepository: ArticleRepository) {
         route("/add") {
             get {
                 call.respond(ThymeleafContent("newArticle", emptyMap() ))
+            }
+            post {
+                val formParams = call.receiveParameters()
+                val name = formParams["name"].toString()
+                val description = formParams["description"].toString()
+                val type = formParams["type"].toString()
+                val price = formParams["price"]?.toFloat() ?: 0F
+                articleRepository.addArticle(Article(name, type, description, price))
+                call.respondRedirect("/article/all")
             }
         }
     }
