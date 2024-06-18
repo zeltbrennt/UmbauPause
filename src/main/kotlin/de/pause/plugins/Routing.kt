48e04fa09033fde5a61f118ccc3521b51e0a7b27
zeltbrennt <zeltbrennt@gmail.com>
2024-06-18 18:40:43 +0200

    little optical touch ups

--- src/main/kotlin/de/pause/plugins/Routing.kt
+++ src/main/kotlin/de/pause/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package de.pause.plugins
 
+import de.pause.getWeekDatesFollowing
 import de.pause.model.Article
 import de.pause.model.ArticleRepository
 import io.ktor.server.application.*
@@ -8,6 +9,8 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
+import java.time.LocalDate
+
 
 fun Application.configureRouting(articleRepository: ArticleRepository) {
 
@@ -21,7 +24,14 @@ fun Application.configureRouting(articleRepository: ArticleRepository) {
         route("/weekly") {
             get {
                 val articles = articleRepository.getCurrentArticles().sortedBy { it.order }
-                call.respond(ThymeleafContent("allArticles", mapOf("articles" to articles)))
+                call.respond(
+                    ThymeleafContent(
+                        "allArticles", mapOf(
+                            "articles" to articles,
+                            "week" to getWeekDatesFollowing(LocalDate.now())
+                        )
+                    )
+                )
             }
         }
         route("/newMenu") {
