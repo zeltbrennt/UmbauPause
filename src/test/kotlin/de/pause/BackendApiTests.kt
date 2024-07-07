53a5997f9301710fc77ff9eeda0a8c2c3a48b180
zeltbrennt <zeltbrennt@gmail.com>
2024-07-07 18:50:11 +0200

    add some test

--- src/test/kotlin/de/pause/BackendApiTests.kt
+++ src/test/kotlin/de/pause/BackendApiTests.kt
@@ -1,19 +1,24 @@
 package de.pause
 
+import de.pause.model.Dish
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import kotlinx.serialization.json.Json
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class BackendApiTests {
     @Test
     fun `current menu endpoint is publicly available`() = testApplication {
-        application {
-
-        }
         val response = client.get("/weekly")
         assertEquals(HttpStatusCode.OK, response.status)
+        val dishes: List<Dish> = Json.decodeFromString(response.bodyAsText())
+        assertEquals(5, dishes.size)
+        val wochentage = listOf("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag")
+        assertContentEquals(wochentage, dishes.map { it.scheduled })
     }
 
     @Test
