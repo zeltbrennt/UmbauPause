19bdc3c500fbc727cdd3a2cebc64acbbf95bc478
zeltbrennt <zeltbrennt@gmail.com>
2024-07-08 21:37:38 +0200

    try to parameterise the urls, not quite working yet

--- frontend/src/components/OrderMenu.tsx
+++ frontend/src/components/OrderMenu.tsx
@@ -3,19 +3,27 @@ import {IMenuItem} from "../util/Interfaces.ts";
 import {List, ListItem} from "@mui/material";
 import MenuItemCard from "./MenuItemCard.tsx";
 
-
 function OrderMenu() {
 
-    const api_url = "http://localhost:8080/weekly" // TODO: URL parametrisieren, da sie in der Docker Umgebung auf das Host System verweist
+    const weekly_menu_url = `http://${import.meta.env.VITE_BACKEND_URL}/weekly`
+    // TODO: URL parametrisieren, da sie in der Docker Umgebung auf das Host System verweist
 
     const [items, setItems] = useState<IMenuItem[]>([])
     const getMenu = async () => {
-        const response = await fetch(api_url)
+        const response = await fetch(weekly_menu_url, {
+            headers: {
+                'AccessControlAllowOrigin': '*'
+            }
+        })
         const data = await response.json()
         setItems(data)
     }
 
     useEffect(() => {
+        console.log(`base url: ${import.meta.env.BASE_URL}`)
+        console.log(`dev: ${import.meta.env.DEV}`)
+        console.log(`VITE_BACKEND: ${import.meta.env.VITE_BACKEND_URL}`)
+        console.log(`fetch from: ${weekly_menu_url}`)
         getMenu().catch((reason) => console.log(`could not fetch menu items: ${reason}`))
     }, [])
     return (
