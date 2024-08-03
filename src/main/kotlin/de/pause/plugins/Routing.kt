7e234519c97658ae88575beaf8754a6213af6e9d
Konrad Klawonn <zeltbrennt@gmail.com>
2024-08-03 13:52:29 +0200

    Refactor: refactor endpoints into sensible groups

--- src/main/kotlin/de/pause/plugins/Routing.kt
+++ src/main/kotlin/de/pause/plugins/Routing.kt
@@ -20,124 +20,139 @@ fun Application.configureRouting(
 ) {
 
     routing {
-        route("/menu") {
-            get {
-                val from = call.request.queryParameters["from"]
-                val formatter = DateTimeFormat.forPattern("yyyy-MM-dd")
-                val day = try {
-                    formatter.parseDateTime(from)
-                } catch (e: IllegalArgumentException) {
-                    return@get call.respond(HttpStatusCode.BadRequest, "Invalid date format")
-                } catch (e: NullPointerException) {
-                    return@get call.respond(HttpStatusCode.BadRequest, "Missing parameter 'from'")
-                }
-
-                val menu = menuRepository.getScheduledMenuFrom(day)
-                call.respond(menu ?: HttpStatusCode.NotFound)
-            }
-        }
-        route("/login") {
-            post {
-                val loginRequest = call.receive<LoginRequest>()
-                val user = userRepository.login(loginRequest)
-                if (user != null) {
-                    val token = createJWT(user)
-                    call.respond(HttpStatusCode.OK, mapOf("accessToken" to token))
-                } else {
-                    call.respond(HttpStatusCode.Unauthorized)
-                }
-            }
-        }
-        route("/register") {
-            post {
-                val loginRequest = call.receive<RegisterRequest>()
-                val success = userRepository.register(loginRequest)
-                when {
-                    success -> call.respond(HttpStatusCode.Created)
-                    else -> call.respond(HttpStatusCode.BadRequest)
-                }
-            }
-        }
-        route("/location") {
-            get {
-                call.respond(orderRepository.getAllLocations())
-            }
-        }
-        authenticate("user") {
-            route("/order") {
-                post {
-                    val jwt = call.principal<JWTPrincipal>()
-                    val user = jwt!!.payload.getClaim("uid").asString()
-                    val temp = call.receive<List<OrderDto>>()
-                    temp.forEach { orderRepository.addOrderByMenuId(it, user) }
-                    //call.application.environment.log.info("user: $user ordered: $temp")
-                    //todo: check if order was successful
-                    call.respond(HttpStatusCode.Created)
-                }
-            }
-            route("/logout") {
-                post {
-                    val jwt = call.principal<JWTPrincipal>()
-                    val user = jwt!!.payload.getClaim("uid").asString()
-                    val success = userRepository.logout(user)
-                    when {
-                        success -> call.respond(HttpStatusCode.OK)
-                        else -> call.respond(HttpStatusCode.BadRequest)
-                    }
-                }
-            }
-        }
-        authenticate("admin") {
-            route("/newDish") {
-                post {
-                    val newDish = call.receive<DishDto>()
-                    dishRepository.addDish(newDish)
-                    call.respond(HttpStatusCode.Created)
-
-                }
-            }
-            route("/dishes") {
-                get {
-                    call.respond(dishRepository.allDishes())
-                }
-            }
-            route("/orderOverview") {
-                get {
-                    val from = call.request.queryParameters["from"]
-                    val formatter = DateTimeFormat.forPattern("yyyy-MM-dd")
-                    val day = try {
-                        formatter.parseDateTime(from)
-                    } catch (e: IllegalArgumentException) {
-                        return@get call.respond(HttpStatusCode.BadRequest, "Invalid date format")
-                    } catch (e: NullPointerException) {
-                        return@get call.respond(HttpStatusCode.BadRequest, "Missing parameter 'from'")
-                    }
-                    val overview = orderRepository.getAllOrdersFrom(day)
-                    call.respond(overview ?: HttpStatusCode.NotFound)
-                }
-            }
-            route("/newMenu") {
-                post {
-
-                    val newMenu = call.receive<MenuInfo>()
-                    call.application.environment.log.info(newMenu.toString())
-                    newMenu.dishes.forEach {
-                        val dish = try {
-                            dishRepository.addDish(DishDto(0, it.name))
-                        } catch (e: ExposedSQLException) {
-                            call.application.environment.log.info("Dish already exists")
-                            dishRepository.findByName(it.name)
+        route("/rest/v1") {
+            route("/info") {
+                route("/menu") {
+                    get {
+                        val from = call.request.queryParameters["from"]
+                        val formatter = DateTimeFormat.forPattern("yyyy-MM-dd")
+                        val day = try {
+                            formatter.parseDateTime(from)
+                        } catch (e: IllegalArgumentException) {
+                            return@get call.respond(HttpStatusCode.BadRequest, "Invalid date format")
+                        } catch (e: NullPointerException) {
+                            return@get call.respond(HttpStatusCode.BadRequest, "Missing parameter 'from'")
                         }
-                        try {
-                            call.application.environment.log.info("new Menu: ${newMenu.validFrom}, ${it.day} , ${dish.id.value}")
-                            menuRepository.addNewMenu(newMenu.validFrom, newMenu.validTo, it.day, dish.id.value)
-                        } catch (e: ExposedSQLException) {
-                            call.application.environment.log.info("Menu already exists")
-                            //call.respond(HttpStatusCode.Conflict)
+
+                        val menu = menuRepository.getScheduledMenuFrom(day)
+                        call.respond(menu ?: HttpStatusCode.NotFound)
+                    }
+                }
+                route("/locations") {
+                    get {
+                        call.respond(orderRepository.getAllLocations())
+                    }
+                }
+                authenticate("admin") {
+                    route("/dishes") {
+                        get {
+                            call.respond(dishRepository.allDishes())
                         }
                     }
-                    call.respond(HttpStatusCode.Created)
                 }
+            }
+            route("/user") {
+                route("/login") {
+                    post {
+                        val loginRequest = call.receive<LoginRequest>()
+                        val user = userRepository.login(loginRequest)
+                        if (user != null) {
+                            val token = createJWT(user)
+                            call.respond(HttpStatusCode.OK, mapOf("accessToken" to token))
+                        } else {
+                            call.respond(HttpStatusCode.Unauthorized)
+                        }
+                    }
+                }
+                route("/register") {
+                    post {
+                        val loginRequest = call.receive<RegisterRequest>()
+                        val success = userRepository.register(loginRequest)
+                        when {
+                            success -> call.respond(HttpStatusCode.Created)
+                            else -> call.respond(HttpStatusCode.BadRequest)
+                        }
+                    }
+                }
+                authenticate("user") {
+                    route("/logout") {
+                        post {
+                            val jwt = call.principal<JWTPrincipal>()
+                            val user = jwt!!.payload.getClaim("uid").asString()
+                            val success = userRepository.logout(user)
+                            when {
+                                success -> call.respond(HttpStatusCode.OK)
+                                else -> call.respond(HttpStatusCode.BadRequest)
+                            }
+                        }
+                    }
+                }
+            }
+            authenticate("user") {
+                route("/order") {
+                    post {
+                        val jwt = call.principal<JWTPrincipal>()
+                        val user = jwt!!.payload.getClaim("uid").asString()
+                        val temp = call.receive<List<OrderDto>>()
+                        temp.forEach { orderRepository.addOrderByMenuId(it, user) }
+                        //call.application.environment.log.info("user: $user ordered: $temp")
+                        //todo: check if order was successful
+                        call.respond(HttpStatusCode.Created)
+                    }
+                }
+            }
+            authenticate("admin") {
+                route("/content") {
+                    route("/add-dish") {
+                        post {
+                            val newDish = call.receive<DishDto>()
+                            dishRepository.addDish(newDish)
+                            call.respond(HttpStatusCode.Created)
+
+                        }
+                    }
+                    route("/new-menu") {
+                        post {
+
+                            val newMenu = call.receive<MenuInfo>()
+                            call.application.environment.log.info(newMenu.toString())
+                            newMenu.dishes.forEach {
+                                val dish = try {
+                                    dishRepository.addDish(DishDto(0, it.name))
+                                } catch (e: ExposedSQLException) {
+                                    call.application.environment.log.info("Dish already exists")
+                                    dishRepository.findByName(it.name)
+                                }
+                                try {
+                                    call.application.environment.log.info("new Menu: ${newMenu.validFrom}, ${it.day} , ${dish.id.value}")
+                                    menuRepository.addNewMenu(newMenu.validFrom, newMenu.validTo, it.day, dish.id.value)
+                                } catch (e: ExposedSQLException) {
+                                    call.application.environment.log.info("Menu already exists")
+                                    //call.respond(HttpStatusCode.Conflict)
+                                }
+                            }
+                            call.respond(HttpStatusCode.Created)
+                        }
+                    }
+                }
+                route("/statistics") {
+                    route("/order-overview") {
+                        get {
+                            val from = call.request.queryParameters["from"]
+                            val formatter = DateTimeFormat.forPattern("yyyy-MM-dd")
+                            val day = try {
+                                formatter.parseDateTime(from)
+                            } catch (e: IllegalArgumentException) {
+                                return@get call.respond(HttpStatusCode.BadRequest, "Invalid date format")
+                            } catch (e: NullPointerException) {
+                                return@get call.respond(HttpStatusCode.BadRequest, "Missing parameter 'from'")
+                            }
+                            val overview = orderRepository.getAllOrdersFrom(day)
+                            call.respond(overview ?: HttpStatusCode.NotFound)
+                        }
+                    }
+                }
+
             }
         }
     }
