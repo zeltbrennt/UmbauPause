707ec469ffceb35bd09bd0c43103d27048d7eb6b
Konrad Klawonn <zeltbrennt@gmail.com>
2024-08-06 15:41:38 +0200

    Make Order: more responsive UI

--- frontend/src/components/ordering/MakeOrder.tsx
+++ frontend/src/components/ordering/MakeOrder.tsx
@@ -1,4 +1,5 @@
 import {
+    Alert,
     Autocomplete,
     Box,
     Button,
@@ -6,8 +7,7 @@ import {
     CardActions,
     CardContent,
     Grid,
-    List,
-    ListItem,
+    Stack,
     TextField,
     Typography
 } from "@mui/material";
@@ -19,13 +19,17 @@ import {getUrlFrom} from "../../util/functions.ts";
 export default function MakeOrder() {
 
     const [orders, setOrders] = useState<Order[]>([])
-    const [selected, setSelected] = useState([false, false, false, false, false])
     const week = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
     const locationUrl = getUrlFrom("info", "locations")
     const [locations, setLocations] = useState<DeliveryLocation[]>([])
     const [menu, setMenu] = useState<MenuInfo>()
     const [validFrom, setValidFrom] = useState("")
     const [validTo, setValidTo] = useState("")
+
+    const [success, setSuccess] = useState(false)
+    const [hasOrdered, setHasOrdered] = useState(false)
+    const [orderInvalid, setOrderInvalid] = useState(false)
+
     const getMenu = async () => {
         let menuUrl = getUrlFrom("info", "menu", dayjs().format("YYYY-MM-DD"))
         let menu: MenuInfo = await fetch(menuUrl)
@@ -68,38 +72,56 @@ export default function MakeOrder() {
                 'Authorization': `Bearer ${sessionStorage.getItem("accessToken")}`
             },
             body: JSON.stringify(orders)
-        }).catch((reason) => console.log(`could not fetch menu items: ${reason}`))
+        })
+            .then(resp => {
+                if (resp.status === 201) {
+                    setSuccess(true)
+                } else {
+                    setSuccess(false)
+                }
+                return resp
+            })
+            .catch((reason) => console.log(`could not fetch menu items: ${reason}`))
+    }
+
+    function handleSubmit() {
+        if (orders.length === 0) {
+            console.log("no orders")
+            setOrderInvalid(true)
+        } else {
+            setOrderInvalid(false)
+            setHasOrdered(true)
+            sendOrder()
+        }
     }
 
     return (
         <Box>
             <Typography variant={"h3"} textAlign={"center"}>Bestellen</Typography>
             <Typography textAlign={"center"}>vom {validFrom} bis {validTo} </Typography>
-            <List>
+            <Stack spacing={2} sx={{marginBottom: 2}}>
                 {orders.map((order, id) => {
                     return (
 
-                        <ListItem key={id}>
-                            <Card>
-                                <CardContent>
-                                    <Typography
-                                        variant={"h6"}>{week[menu?.dishes.find(d => d.id === order.item).day - 1]}</Typography>
-                                    <Typography> {menu?.dishes.find(d => d.id === order.item).name}</Typography>
-                                    <Typography
-                                        variant={"caption"}>{locations.find(l => order.location === l.id).name}</Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button
-                                        onClick={() => setOrders(orders.filter(o => !(o.item === order.item && o.location === order.location)))}
-                                        variant={"outlined"}>entfernen</Button>
-                                </CardActions>
-                            </Card>
-                        </ListItem>
+                        <Card sx={{width: '100%'}}>
+                            <CardContent>
+                                <Typography
+                                    variant={"h6"}>{week[menu?.dishes.find(d => d.id === order.item).day - 1]}</Typography>
+                                <Typography> {menu?.dishes.find(d => d.id === order.item).name}</Typography>
+                                <Typography
+                                    variant={"caption"}>{locations.find(l => order.location === l.id).name}</Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button
+                                    onClick={() => setOrders(orders.filter(o => !(o.item === order.item && o.location === order.location)))}
+                                    variant={"outlined"}>entfernen</Button>
+                            </CardActions>
+                        </Card>
 
 
                     )
                 })}
-            </List>
+            </Stack>
             <Grid container spacing={2}>
                 <Grid item sm={6}>
                     <Autocomplete
@@ -108,14 +130,14 @@ export default function MakeOrder() {
                         onChange={(event, value) => {
                             setCurrentSelectedMenuItem(value as MenuItem)
                         }}
-                        renderInput={(params) => <TextField {...params}/>}>
+                        renderInput={(params) => <TextField {...params} size={'small'}/>}>
                     </Autocomplete>
                 </Grid>
                 <Grid item sm={4}>
                     <Autocomplete
                         options={locations}
                         getOptionLabel={(option: DeliveryLocation) => option.name}
-                        renderInput={(params) => <TextField {...params}/>}
+                        renderInput={(params) => <TextField {...params} size={'small'}/>}
                         onChange={(event, value: DeliveryLocation) => {
                             setCurrentSelectedLocation(value as DeliveryLocation)
                         }}>
@@ -129,10 +151,19 @@ export default function MakeOrder() {
                                 location: currentSelectedLocation?.id ?? 0
                             }].sort((a, b) => a.item - b.item))
                         }
-                    }} variant={"outlined"}>Hinzufügen</Button>
+                    }} variant={"outlined"} size={"medium"}>Hinzufügen</Button>
                 </Grid>
             </Grid>
-            <Button onClick={sendOrder} variant={"contained"}>Bestellung abschicken</Button>
+
+            <Button fullWidth sx={{marginTop: 2, marginBottom: 2}} onClick={handleSubmit} variant={"contained"}>Bestellung
+                abschicken</Button>
+            <Stack spacing={2}>
+                {(hasOrdered && success) ? <Alert severity="success">Bestellung erfolgreich</Alert> : null}
+                {(hasOrdered && !success) ? <Alert severity="error">Bestellung fehlgeschlagen</Alert> : null}
+                {orderInvalid ?
+                    <Alert severity={"info"}>Bitte wähle mindestens ein Gericht von der Karte</Alert> : null}
+            </Stack>
+
         </Box>
     )
 }
