0dd5e758104cb1e3555e7cc3b47b51c5d34bbcf7
Konrad Klawonn <zeltbrennt@gmail.com>
2024-08-19 21:25:00 +0200

    Refresh: create Wrapper Function to use Refresh token

--- frontend/src/util/functions.ts
+++ frontend/src/util/functions.ts
@@ -1,4 +1,6 @@
 import {BASE_URL} from "./constants.ts";
+import {jwtDecode} from "jwt-decode";
+import {JWTToken} from "./Interfaces.ts";
 
 export function mapDayOfWeek(day: number): string {
     switch (day) {
@@ -23,4 +25,43 @@ export function mapDayOfWeek(day: number): string {
 
 export function getUrlFrom(...path: string[]): string {
     return BASE_URL + path.join("/")
+}
+
+function isAccessTokenValid() {
+    if (!sessionStorage.getItem("accessToken")) {
+        console.log("has no token")
+        return false
+    } else {
+        console.log("has token")
+        const decodedToken = jwtDecode<JWTToken>(sessionStorage.getItem("accessToken"))
+        return decodedToken.exp * 1000 > Date.now()
+    }
+}
+
+function saveNewAccessToken() {
+    fetch(getUrlFrom("refresh"), {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+            "Authorization": `Bearer ${sessionStorage.getItem("refreshToken")}`
+        }
+    }).then(response => {
+        if (response.ok) {
+            response.json().then(data => {
+                sessionStorage.setItem("accessToken", data.accessToken)
+                console.log("Token refreshed")
+            })
+        } else {
+            console.log("Token refresh failed")
+            sessionStorage.clear()
+            location.href = "/"
+        }
+    })
+}
+
+export async function withTokenRefresh(fetchFunction: () => Promise<Response>): Promise<Response> {
+    if (!isAccessTokenValid()) {
+        await saveNewAccessToken()
+    }
+    return fetchFunction()
 }
